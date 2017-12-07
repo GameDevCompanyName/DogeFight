159cde5cb4fd2f9935e19720c19ec73a2e61c1bb
danil <danilaeremenko@mail.ru>
2017-12-08 02:20:47 +0300

    Непофишенный гейм

--- src/GameDevCompanyName/Game/Game.java
+++ src/GameDevCompanyName/Game/Game.java
@@ -1,21 +1,28 @@
 package GameDevCompanyName.Game;
 
+import GameDevCompanyName.Game.Graphics.Graphics;
+import GameDevCompanyName.Game.Logic.Heroes;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Game extends Application{
+public class Game extends Application {
+
+    private static final boolean DEBUG = false;
+    private Graphics graphics;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
-
-
-
+        Heroes heros = Heroes.init();
+        graphics = new Graphics(heros.getHeroes(), DEBUG);
+        graphics.start();
 
     }
+
 
     public static void main(String[] args) {
-
+        launch(args);
     }
+
 
 }
