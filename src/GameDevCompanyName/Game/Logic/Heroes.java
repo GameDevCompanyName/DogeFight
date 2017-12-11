6c3d474301ac7097e2f1a28164e3b9d8c038b672
Игорь <9440533@gmail.com>
2017-12-11 15:46:10 +0300

    Добавил реализацию метода getHeros

--- src/GameDevCompanyName/Game/Logic/Heroes.java
+++ src/GameDevCompanyName/Game/Logic/Heroes.java
@@ -1,10 +1,28 @@
 package GameDevCompanyName.Game.Logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Heroes {
 
+    private List<Character> heroes = new ArrayList<>();
+
     public List<Character> getHeroes(){
+        heroes.add(new Character("...",
+                "Dogger",
+                10,
+                10,
+                10,
+                100,
+                100));
+        heroes.add(new Character("...",
+                "PESEL'",
+                10,
+                10,
+                10,
+                100,
+                100));
+        return heroes;
 
     }
 
