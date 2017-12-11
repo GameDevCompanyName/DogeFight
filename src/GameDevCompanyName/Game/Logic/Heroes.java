110ddb373c4ca589e58352ad27f7f94b5d70f4c2
danil <danilaeremenko@mail.ru>
2017-12-11 15:47:17 +0300

    Пустое тело

--- src/GameDevCompanyName/Game/Logic/Heroes.java
+++ src/GameDevCompanyName/Game/Logic/Heroes.java
@@ -1,11 +1,13 @@
 package GameDevCompanyName.Game.Logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Heroes {
 
     public List<Character> getHeroes(){
-
+        //TODO
+        return new ArrayList<>();
     }
 
 }
