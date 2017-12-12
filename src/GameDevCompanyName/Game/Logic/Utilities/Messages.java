046f2d1c4febc285feb5e5626a1792905c1f9c7f
Игорь <9440533@gmail.com>
2017-12-12 18:52:40 +0300

    Пофиксил текст выводимых сообщений

--- src/GameDevCompanyName/Game/Logic/Utilities/Messages.java
+++ src/GameDevCompanyName/Game/Logic/Utilities/Messages.java
@@ -3,10 +3,10 @@ package GameDevCompanyName.Game.Logic.Utilities;
 public class Messages {
 
     //Фразы, используемые в бою
-    private static final String CHARACTER_DODGED = " увернулся от удара!";
+    private static final String CHARACTER_DODGED = " увернулся от удара.";
     private static final String CHARACTER_BLOCKED = " заблокировал удар.";
-    private static final String CHARACTER_BLOCK_FAILED = " увернулся от удара!";
-    private static final String CHARACTER_DAMAGED = " увернулся от удара!";
+    private static final String CHARACTER_BLOCK_FAILED = " не смог заблокировать удар и получил урона: ";
+    private static final String CHARACTER_DAMAGED = " получил урона: ";
     private static final String CHARACTER_MISTURN = " переводит дух и копит силы.";
 
 
